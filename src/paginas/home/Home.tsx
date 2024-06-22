27640ca441c337bb7a6827301b6b4e8b78062f77
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-06-22 10:40:46 -0300

    task: iniciando o projeto com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/p5iXQGv.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
