4594098fd3693e5f14e2512fcfc026786615a825
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-06-24 13:12:21 -0300

    task: utilizando renderização condicional e useEffect/hook

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
 import './Home.css';
+import { useState } from 'react';
 
-function Home(){
+function Home() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
     return (
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/p5iXQGv.jpg" alt="Imagem Tela Inicial" className="img"/>
-        </>
+        <div>
+            {loggedIn ? (
+                <>
+                    <h1 className="titulo" >Bem-vindo de volta!</h1>
+                    <h1 className="titulo">Home</h1>
+                    <img src="https://i.imgur.com/p5iXQGv.jpg" alt="Imagem Tela Inicial" className="img" />
+                </>
+            ) : (
+                <button onClick={() => setLoggedIn(true)}>Entrar</button>
+            )}
+        </div>
     );
 }
 
